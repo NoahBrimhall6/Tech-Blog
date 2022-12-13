d23d0cab802dfa4af8e0ec8c573e09018434614c
Noah Brimhall <noahbrimhall6@gmail.com>
2022-12-13 13:10:59 -0700

    Created update and delete routes for comments and posts

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,6 +1,7 @@
 const router =  require('express').Router();
 const { Comment } = require('../../models');
 
+// Create a comment
 router.post('/', async (req, res) => {
   try {
     const commentData = await Comment.create({
@@ -16,6 +17,33 @@ router.post('/', async (req, res) => {
   }
 
   console.log(`Comment: ${req.body.comment}  user_id: ${req.session.userId}  post_id: ${req.body.post_id}`);
+});
+
+// Update a comment
+router.put('/:id', async (req, res) => {
+  try {
+    const commentData = await Comment.update(
+      { comment: req.body.comment },
+      { where: { id: req.params.id } }
+    );
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+    console.error('Unable to update comment');
+  }
+});
+
+//Delete a comment
+router.delete('/:id', async (req, res) => {
+  try {
+    const commentData = await Comment.destroy({ where: { id: req.params.id } });
+
+    res.status(200).json(commentData);
+  } catch (err) {
+    res.status(500).json(err);
+    console.error('Unable to delete comment.');
+  }
 });
 
 module.exports = router;
--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
 
+// Create a post
 router.post('/', async (req, res) => {
   try {
     const postData = await Post.create({
@@ -12,6 +13,36 @@ router.post('/', async (req, res) => {
   } catch (err) {
     res.status(500).json(err);
     console.error('Failed to create post.');
+  }
+});
+
+// Update a post
+router.put('/:id', async (req, res) => {
+  try {
+    const postData = await Post.update(
+      { 
+        title: req.body.title,
+        body: req.body.body
+      },
+      { where: { id: req.params.id } }
+    );
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log('Failed to update post.');
+  }
+});
+
+// Delete a post
+router.delete('/:id', async (req, res) => {
+  try {
+    const postData = await Post.destroy({ where: { id: req.params.id } });
+
+    res.status(200).json(postData);
+  } catch (err) {
+    res.status(500).json(err);
+    console.log('Failed to update post.');
   }
 });
 
