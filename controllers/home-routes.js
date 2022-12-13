a749e6c52473fe0eb8b954c7a082e1bc5fb8d549
Noah Brimhall <noahbrimhall6@gmail.com>
2022-12-12 17:34:44 -0700

    Updated cards in dashboard.handlebars and homepage.handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -5,8 +5,9 @@ const withAuth = require('../utils/auth');
 // Render the homepage
 router.get('/', async (req, res) => {
   try {
-    const postData = await Post.findAll();
+    const postData = await Post.findAll({ include: User });
     const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts);
 
     res.render('homepage', { posts, loggedIn: req.session.loggedIn });
   } catch (err) {
@@ -18,7 +19,10 @@ router.get('/', async (req, res) => {
 // Render the dashboard
 router.get('/dashboard', withAuth, async (req, res) => {
   try {
-    const postData = await Post.findAll();
+    const postData = await Post.findAll({
+      where: { user_id: req.session.userId },
+      include: User
+    });
     const posts = postData.map((post) => post.get({ plain: true }));
 
     res.render('dashboard', { posts, loggedIn: req.session.loggedIn });
