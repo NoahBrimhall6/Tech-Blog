499d47ff1e6e0eceb2b94537f13d45e01cdf25e5
Noah Brimhall <noahbrimhall6@gmail.com>
2022-12-13 18:43:07 -0700

    Added ability to update and delete posts

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,3 +1,4 @@
+// Create Post
 $('#postBtn').click(async (event) => {
   event.preventDefault();
 
@@ -22,7 +23,52 @@ $('#postBtn').click(async (event) => {
   if (response.ok) {
     location.replace('/dashboard');
   } else {
-    $('.alert').text('Failed to login').css('display', 'flex');  
-    return;
+    $('.alert').text('Failed to create post').css('display', 'flex');  
   }
+});
+
+// Delete Post
+$('.deleteBtn').click(async (event) => {
+  event.preventDefault();
+  const postId = $(event.target).parent().parent().attr('id');
+  console.log(postId);
+
+  const response = await fetch(`api/posts/${postId}`, {
+    method: 'DELETE',
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (response.ok) {
+    location.replace('/dashboard');
+  } else {
+    $('.alert').text('Failed to delete post').css('display', 'flex');  
+  }
+});
+
+// Update post
+$('.updateBtn').click(async (event) => {
+  event.preventDefault();
+  const postId = $(event.target).parent().parent().attr('id');
+  const newTitle = $(`#title-${postId}`).text();
+  const newBody = $(`#body-${postId}`).text();
+
+  const response = await fetch(`api/posts/${postId}`, {
+    method: 'PUT',
+    body: JSON.stringify({ title: newTitle, body: newBody }),
+    headers: { 'Content-Type': 'application/json' }
+  });
+
+  if (response.ok) {
+    location.replace('/dashboard');
+    console.log(response);
+  } else {
+    $('.alert').text('Failed to update post').css('display', 'flex');  
+  }
+});
+
+// Click on post to see comments
+$('.commentBtn').click(function (event) {
+  event.preventDefault();
+  const postId = $(event.target).parent().parent().attr('id');
+  document.location.replace(`/post/${postId}`);
 });
--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,6 +1,5 @@
 $('.card').click(function (event) {
   const postId = $(this).attr('id');
-  console.log(postId);
   document.location.replace(`/post/${postId}`);
 });
 
